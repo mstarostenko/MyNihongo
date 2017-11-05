d2e94fb2d11827c8276e80c4c8fe7e0bed75bf95
Maxim Starostenko <m.starostenko@office.ngs.ru>
2017-11-05 15:53:48 +0700

    Подключил иконки

--- resources/layouts/default.vue
+++ resources/layouts/default.vue
@@ -1,84 +1,88 @@
-<template>
-  <v-app dark>
-    <v-navigation-drawer
+<template lang="pug">
+  v-app(
+    dark
+  )
+    v-navigation-drawer(
       persistent
       :mini-variant="miniVariant"
       :clipped="clipped"
       v-model="drawer"
       app
-    >
-      <v-list>
-        <v-list-tile 
+    )
+
+      v-list
+        v-list-tile(
           router
           :to="item.to"
           :key="i"
           v-for="(item, i) in items"
           exact
-        >
-          <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar fixed app :clipped-left="clipped">
-      <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn 
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>menu</v-icon>
-      </v-btn>
-    </v-toolbar>
-    <main>
-      <v-content>
-        <v-container>
-          <nuxt />
-        </v-container>
-      </v-content>
-    </main>
-    <v-navigation-drawer
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-      app
-    >
-      <v-list>
-        <v-list-tile @click.native="right = !right">
-          <v-list-tile-action>
-            <v-icon light>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; 2017</span>
-    </v-footer>
-  </v-app>
+        )
+          v-list-tile-action
+            v-icon(
+               v-html="item.icon"
+            )
+
+          v-list-tile-content
+            v-list-tile-title(
+              v-text="item.title"
+            )
+
+  <!--   <v&#45;toolbar fixed app :clipped&#45;left="clipped"> -->
+  <!--     <v&#45;toolbar&#45;side&#45;icon @click="drawer = !drawer"></v&#45;toolbar&#45;side&#45;icon> -->
+  <!--     <v&#45;btn  -->
+  <!--       icon -->
+  <!--       @click.stop="miniVariant = !miniVariant" -->
+  <!--     > -->
+  <!--       <v&#45;icon v&#45;html="miniVariant ? 'chevron_right' : 'chevron_left'"></v&#45;icon> -->
+  <!--     </v&#45;btn> -->
+  <!--     <v&#45;btn -->
+  <!--       icon -->
+  <!--       @click.stop="clipped = !clipped" -->
+  <!--     > -->
+  <!--       <v&#45;icon>web</v&#45;icon> -->
+  <!--     </v&#45;btn> -->
+  <!--     <v&#45;btn -->
+  <!--       icon -->
+  <!--       @click.stop="fixed = !fixed" -->
+  <!--     > -->
+  <!--       <v&#45;icon>remove</v&#45;icon> -->
+  <!--     </v&#45;btn> -->
+  <!--     <v&#45;toolbar&#45;title v&#45;text="title"></v&#45;toolbar&#45;title> -->
+  <!--     <v&#45;spacer></v&#45;spacer> -->
+  <!--     <v&#45;btn -->
+  <!--       icon -->
+  <!--       @click.stop="rightDrawer = !rightDrawer" -->
+  <!--     > -->
+  <!--       <v&#45;icon>menu</v&#45;icon> -->
+  <!--     </v&#45;btn> -->
+  <!--   </v&#45;toolbar> -->
+  <!--   <main> -->
+  <!--     <v&#45;content> -->
+  <!--       <v&#45;container> -->
+  <!--         <nuxt /> -->
+  <!--       </v&#45;container> -->
+  <!--     </v&#45;content> -->
+  <!--   </main> -->
+  <!--   <v&#45;navigation&#45;drawer -->
+  <!--     temporary -->
+  <!--     :right="right" -->
+  <!--     v&#45;model="rightDrawer" -->
+  <!--     app -->
+  <!--   > -->
+  <!--     <v&#45;list> -->
+  <!--       <v&#45;list&#45;tile @click.native="right = !right"> -->
+  <!--         <v&#45;list&#45;tile&#45;action> -->
+  <!--           <v&#45;icon light>compare_arrows</v&#45;icon> -->
+  <!--         </v&#45;list&#45;tile&#45;action> -->
+  <!--         <v&#45;list&#45;tile&#45;title>Switch drawer (click me)</v&#45;list&#45;tile&#45;title> -->
+  <!--       </v&#45;list&#45;tile> -->
+  <!--     </v&#45;list> -->
+  <!--   </v&#45;navigation&#45;drawer> -->
+  <!--   <v&#45;footer :fixed="fixed" app> -->
+  <!--     <span>&#38;copy; 2017</span> -->
+  <!--   </v&#45;footer> -->
+  <!-- </v&#45;app> -->
 </template>
 
 <script>
